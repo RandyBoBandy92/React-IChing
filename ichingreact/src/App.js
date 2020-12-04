e936a2fccc9fc329a4925d6c24b089e533ce825b
RandyBoBandy <ryan.gulak@gmail.com>
2020-12-03 17:39:16 -0800

    ok i cant get my checktrigrams to work right

--- ichingreact/src/App.js
+++ ichingreact/src/App.js
@@ -1,82 +1,143 @@
 import React from "react";
-import iChingData from "./iChingData";
+import { iChingData, getTrigram } from "./iChingData";
+
+
 
 class Line extends React.Component {
   constructor(props) {
-    super(props);
-    this.state = {
-      imgSrc: "../images/nothing.png",
-      value: undefined,
-    };
+    super(props)
     this.styles = {
       width: "200px",
-      border: "2px solid black",
-    };
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  getRandomNumber(min, max) {
-    // console.log(Math.floor(Math.random() * (max - min + 1)) + min)
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-
-  getImagePath(number) {
-    if (number === 6) {
-      return "../images/old_yin.png";
-    } else if (number === 7) {
-      return "../images/young_yang.png";
-    } else if (number === 8) {
-      return "../images/young_yin.png";
-    } else if (number === 9) {
-      return "../images/old_yang.png";
-    } else {
-      return "../images/nothing.png";
+      border: "2px solid black"
     }
   }
-
-  handleClick() {
-    // this.setState({imgSrc: '../images/young_yang.png'})
-    let value = this.getRandomNumber(6, 9);
-    let imagePath = this.getImagePath(value);
-    this.setState({
-      value: value,
-      imgSrc: imagePath,
-    });
-  }
-
-  render() {
-    console.log(iChingData)
+  render () {
     return (
       <div>
         <img
-          onClick={this.handleClick}
-          style={this.styles}
-          src={this.state.imgSrc}
+        src={this.props.line.image}
+        style={this.styles}
+        onClick={() => this.props.handleLineClick(this.props.line.lineNum)}
         />
       </div>
-    );
+    )
   }
 }
-
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = iChingData
     this.styles = {
       margin: "auto",
       width: "max-content",
       marginTop: "20px"
     }
+    this.handleLineClick = this.handleLineClick.bind(this)
+
   }
 
+  makeLinesReadableToHumanPls(linesArray) {
+    return {
+      line1: linesArray[5],
+      line2: linesArray[4],
+      line3: linesArray[3],
+      line4: linesArray[2],
+      line5: linesArray[1],
+      line6: linesArray[0],
+    }
+  }
+
+
+  checkTrigrams() {
+    let lines = this.makeLinesReadableToHumanPls(this.state.lines)
+    this.lowerTrigram = undefined
+    this.upperTrigram = undefined
+
+    if (lines.line1.value && lines.line2.value && lines.line3.value) { // if the 3 bottom lines have values
+      console.log("Bottom trigram has value!")
+      this.lowerTrigram = getTrigram(lines.line1.name, lines.line2.name, lines.line3.name)
+      console.log(this.lowerTrigram)
+    }
+    if (lines.line4.value && lines.line5.value && lines.line6.value) { // if the 3 upper lines have values
+      console.log("Upper trigram has value!")
+      this.upperTrigram = getTrigram(lines.line4.name, lines.line5.name, lines.line6.name)
+      console.log(this.upperTrigram)
+    }
+  }
+
+  updateTrigramsState(lowerTrigram, upperTrigram) {
+    this.setState(prevState => {
+      return {
+        ...prevState,
+        trigrams: {
+          upper: {
+            value: upperTrigram
+          },
+          lower: {
+            value: lowerTrigram
+          }
+        }
+      }
+    })
+  }
+
+
+  handleLineClick(lineNum) {
+    // console.log(`Click detected on ${lineNum}`)
+    let newValue = this.getRandomNumber(6, 9)
+    let newPath = this.getImagePath(newValue)
+    let newName = this.getName(newValue) 
+    this.setState(prevState => {
+      const updatedLines = prevState.lines.map(line => {
+        if (line.lineNum === lineNum) {
+          return {
+            ...line,
+            value: newValue,
+            image: newPath,
+            name: newName
+          }
+        }
+        return line
+      })
+      return {
+        ...prevState,
+        lines: updatedLines
+      }
+    })
+    this.checkTrigrams()
+  }
+
+  getName(value) {
+    if (value === 6 || value === 8) {
+      return 'yin'
+    } else {
+      return 'yang'
+    }
+  }
+
+  getImagePath(value) {
+    if (value === 6) {
+      return this.state.imagePaths.oldYin
+    } else if (value === 7) {
+      return this.state.imagePaths.youngYang
+    } else if (value === 8) {
+      return this.state.imagePaths.youngYin
+    } else if (value === 9) {
+      return this.state.imagePaths.oldYang
+    }
+  }
+  
+  getRandomNumber(min, max) {
+    // console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
   render() {
+    const lineComponents = this.state.lines.map(line => <Line line={line} handleLineClick={this.handleLineClick}
+    image={line.image}
+    />)
     return (
       <div style={this.styles} className="app">
-        <Line />
-        <Line />
-        <Line />
-        <Line />
-        <Line />
-        <Line />
+        {lineComponents}
       </div>
     );
   }
--- ichingreact/src/iChingData.js
+++ ichingreact/src/iChingData.js
@@ -3,36 +3,50 @@
 // Trigrams
 // Hexagrams
 
-const iChingData = {
-    lines: {
-        line1: {
+export const iChingData = {
+    lines: [
+        {value: undefined, lineNum: 6, id: 6, image: '../images/nothing.png', name: undefined},
+        {value: undefined, lineNum: 5, id: 5, image: '../images/nothing.png', name: undefined},
+        {value: undefined, lineNum: 4, id: 4, image: '../images/nothing.png', name: undefined},
+        {value: undefined, lineNum: 3, id: 3, image: '../images/nothing.png', name: undefined},
+        {value: undefined, lineNum: 2, id: 2, image: '../images/nothing.png', name: undefined},
+        {value: undefined, lineNum: 1, id: 1, image: '../images/nothing.png', name: undefined},
+           ],
+    imagePaths: {
+        oldYin: '../images/old_yin.png',
+        oldYang: '../images/old_yang.png',
+        youngYin: '../images/young_yin.png',
+        youngYang: '../images/young_yang.png',
+        nothing: '../images/nothing.png'
+    },
+    trigrams: {
+        upper: {
             value: undefined
         },
-        line2: {
+        lower: {
             value: undefined
-        },
-        line3: {
-            value: undefined
-        },
-        line4: {
-            value: undefined
-        },
-        line5: {
-            value: undefined
-        },
-        line6: {
-            value: undefined
-        },
-        imagePaths: {
-            oldYin: '../images/old_yin.png',
-            oldYang: '../images/old_yang.png',
-            youngYin: '../images/young_yin.png',
-            youngYang: '../images/young_yang.png',
-            nothing: '../images/nothing.png'
         }
     }
 }
 
 
-
-export default iChingData
+export function getTrigram(lowerLineValue, middleLineValue, upperLineValue) {
+    // so lets start with thunder
+    if (upperLineValue === 'yin' && middleLineValue === 'yin' && lowerLineValue === 'yang') {
+        return 'thunder'
+    } else if (upperLineValue === 'yang' && middleLineValue === 'yang' && lowerLineValue === 'yin') {
+        return 'wind'
+    } else if (upperLineValue === 'yang' && middleLineValue === 'yin' && lowerLineValue === 'yang') {
+        return 'fire'
+    } else if (upperLineValue === 'yin' && middleLineValue === 'yin' && lowerLineValue === 'yin') {
+        return 'earth'
+    } else if (upperLineValue === 'yin' && middleLineValue === 'yang' && lowerLineValue === 'yang') {
+        return 'lake'
+    } else if (upperLineValue === 'yang' && middleLineValue === 'yang' && lowerLineValue ==='yang') {
+        return 'heaven'
+    } else if (upperLineValue === 'yin' && middleLineValue === 'yang' && lowerLineValue === 'yin') {
+        return 'water'
+    } else if (upperLineValue === 'yang' && middleLineValue === 'yin' && lowerLineValue === 'yin') {
+        return 'mountain'
+    }
+}
