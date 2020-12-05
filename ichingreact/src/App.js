85dd97f96e10acef61247563b40ab8e0cb69bbdf
RandyBoBandy <ryan.gulak@gmail.com>
2020-12-04 17:31:25 -0800

    added hexagram json files. having trouble with dynamic styling right now

--- ichingreact/src/App.js
+++ ichingreact/src/App.js
@@ -1,56 +1,98 @@
 import React from "react";
-import { iChingData, getTrigram } from "./iChingData";
-
+import { iChingData, getTrigram, getHexagramNumber, getTransformValue } from "./iChingData";
 
 class IChingText extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
   }
   render() {
     return (
       <div>
         <h3>Upper Trigram: {this.props.divinationData.trigrams.upper.value}</h3>
         <h3>Lower Trigram: {this.props.divinationData.trigrams.lower.value}</h3>
+        <h3>Hexagram Number: {this.props.divinationData.hexagram.number}</h3>
       </div>
-    )
+    );
   }
 }
 class Line extends React.Component {
   constructor(props) {
-    super(props)
-    this.styles = {
-      width: "200px",
-      border: "2px solid black"
+    super(props);
+    this.primaryStyles = {
+      // width: "200px",
+      border: "2px solid black",
+      margin: "2px"
+    };
+    this.transformedStyles = {
+      ...this.primaryStyles,
+      display: "none"
+    }
+    this.addStylestoState(this.primaryStyles, this.transformedStyles)
+  }
+  addStylestoState(primaryStyles, transformedStyles) {
+    this.setState(prevState => {
+      return {
+        ...prevState,
+        lineStyles: {
+          primaryStyles: primaryStyles,
+          transFormedStyles: transformedStyles
+        }
+      }
+    })
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.props.hexagram.number !== this.props.hexagram.transformNumber) {
+      this.setState(prevState => {
+        return {
+          ...prevState,
+          lineStyles: {
+            ...prevState.lineStyles,
+            transFormedStyles: {
+              display: "block"
+            }
+          }
+        }
+      })
     }
   }
-  render () {
+
+  render() {
+
     return (
-      <div className={`line-${this.props.line.lineNum}`}>
+      <div className={`line line-${this.props.line.lineNum}`}>
         <img
-        src={this.props.line.image}
-        style={this.styles}
-        onClick={() => this.props.handleLineClick(this.props.line.lineNum)}
+          className="primary-hexagram"
+          src={this.props.line.image}
+          style={this.primaryStyles}
+          onClick={() => this.props.handleLineClick(this.props.line.lineNum)}
+        />
+        <img
+          className="transformed-hexagram"
+          src={this.props.line.transformImage}
+          style={this.transformedStyles}
         />
       </div>
-    )
+    );
   }
 }
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = iChingData
+    this.state = iChingData;
     this.styles = {
       margin: "auto",
       width: "max-content",
-      marginTop: "20px"
-    }
-    this.handleLineClick = this.handleLineClick.bind(this)
-
+      marginTop: "20px",
+    };
+    this.handleLineClick = this.handleLineClick.bind(this);
   }
 
   componentDidUpdate(prevProps, prevState) {
     if (prevState.lines !== this.state.lines) {
-      this.checkTrigrams()
+      this.checkTrigrams();
+    } else if (prevState.trigrams !== this.state.trigrams) {
+      this.checkHexagram();
     }
   }
 
@@ -62,101 +104,173 @@ class App extends React.Component {
       line4: linesArray[2],
       line5: linesArray[1],
       line6: linesArray[0],
+    };
+  }
+
+  checkHexagram() {
+    // console.log("check hexagram function running");
+    if (this.state.trigrams.lower.value && this.state.trigrams.upper.value) {
+      // if both trigrams are truthy - have values
+      let hexagramNumber = getHexagramNumber(
+        this.state.trigrams.lower.value,
+        this.state.trigrams.upper.value
+      );
+      let transformedHexagramNumber = getHexagramNumber(
+        this.state.trigrams.transformedLower.value,
+        this.state.trigrams.transformedUpper.value
+
+      );
+      this.updateHexagramState(hexagramNumber, transformedHexagramNumber);
+    } else {
+      // console.log("hexagram cannot be generated yet");
     }
   }
 
+
+  updateHexagramState(hexagramNumber, transformedHexagramNumber) {
+    this.setState((prevState) => {
+      return {
+        ...prevState,
+        hexagram: {
+          number: hexagramNumber,
+          transformNumber: transformedHexagramNumber
+        },
+      };
+    });
+  }
 
   checkTrigrams() {
-    let lines = this.makeLinesReadableToHumanPls(this.state.lines)
-    this.lowerTrigram = undefined
-    this.upperTrigram = undefined
+    let lines = this.makeLinesReadableToHumanPls(this.state.lines);
+    let lowerTrigram 
+    let upperTrigram 
+    let lowerTransformTrigram
+    let upperTransformTrigram
 
-    if (lines.line1.value && lines.line2.value && lines.line3.value) { // if the 3 bottom lines have values
-      console.log("Bottom trigram has value!")
-      this.lowerTrigram = getTrigram(lines.line1.name, lines.line2.name, lines.line3.name)
-      console.log(this.lowerTrigram)
+    if (lines.line1.value && lines.line2.value && lines.line3.value) {
+      // if the 3 bottom lines have values
+      // console.log("Bottom trigram has value!");
+      lowerTrigram = getTrigram(
+        lines.line1.name,
+        lines.line2.name,
+        lines.line3.name
+      );
+      lowerTransformTrigram = getTrigram(
+        lines.line1.transformName,
+        lines.line2.transformName,
+        lines.line3.transformName,
+      );
+      // console.log(this.lowerTrigram);
     }
-    if (lines.line4.value && lines.line5.value && lines.line6.value) { // if the 3 upper lines have values
-      console.log("Upper trigram has value!")
-      this.upperTrigram = getTrigram(lines.line4.name, lines.line5.name, lines.line6.name)
-      console.log(this.upperTrigram)
+    if (lines.line4.value && lines.line5.value && lines.line6.value) {
+      // if the 3 upper lines have values
+      // console.log("Upper trigram has value!");
+      upperTrigram = getTrigram(
+        lines.line4.name,
+        lines.line5.name,
+        lines.line6.name
+      );
+      upperTransformTrigram = getTrigram(
+        lines.line4.transformName,
+        lines.line5.transformName,
+        lines.line6.transformName
+      );
+      // console.log(this.upperTrigram);
     }
-    this.updateTrigramsState(this.lowerTrigram, this.upperTrigram)
+    this.updateTrigramsState(lowerTrigram, upperTrigram, lowerTransformTrigram, upperTransformTrigram);
   }
 
-  updateTrigramsState(lowerTrigram, upperTrigram) {
-    this.setState(prevState => {
+  updateTrigramsState(lowerTrigram, upperTrigram, lowerTransformTrigram, upperTransformTrigram) {
+    this.setState((prevState) => {
       return {
         ...prevState,
         trigrams: {
           upper: {
-            value: upperTrigram
+            value: upperTrigram,
           },
           lower: {
-            value: lowerTrigram
+            value: lowerTrigram,
+          },
+          transformedUpper: {
+            value: upperTransformTrigram
+          },
+          transformedLower: {
+            value: lowerTransformTrigram
           }
-        }
-      }
-    })
+        },
+      };
+    });
   }
-
 
   handleLineClick(lineNum) {
     // console.log(`Click detected on ${lineNum}`)
-    let newValue = this.getRandomNumber(6, 9)
-    let newPath = this.getImagePath(newValue)
-    let newName = this.getName(newValue) 
-    this.setState(prevState => {
-      const updatedLines = prevState.lines.map(line => {
+    let newValue = this.getRandomNumber(6, 9);
+    let newPath = this.getImagePath(newValue);
+    let newName = this.getName(newValue);
+    let newTransformValue = getTransformValue(newValue)
+    let newTransformPath = this.getImagePath(newTransformValue)
+    let newTransformName = this.getName(newTransformValue)
+    // i need to write the function first and import it 
+    this.setState((prevState) => {
+      const updatedLines = prevState.lines.map((line) => {
         if (line.lineNum === lineNum) {
           return {
             ...line,
             value: newValue,
+            transformValue: newTransformValue,
             image: newPath,
-            name: newName
-          }
+            transformImage: newTransformPath,
+            name: newName,
+            transformName: newTransformName
+          };
         }
-        return line
-      })
+        return line;
+      });
       return {
         ...prevState,
-        lines: updatedLines
-      }
-    })
+        lines: updatedLines,
+      };
+    });
   }
 
   getName(value) {
     if (value === 6 || value === 8) {
-      return 'yin'
+      return "yin";
     } else {
-      return 'yang'
+      return "yang";
     }
   }
 
   getImagePath(value) {
     if (value === 6) {
-      return this.state.imagePaths.oldYin
+      return this.state.imagePaths.oldYin;
     } else if (value === 7) {
-      return this.state.imagePaths.youngYang
+      return this.state.imagePaths.youngYang;
     } else if (value === 8) {
-      return this.state.imagePaths.youngYin
+      return this.state.imagePaths.youngYin;
     } else if (value === 9) {
-      return this.state.imagePaths.oldYang
+      return this.state.imagePaths.oldYang;
     }
   }
-  
+
   getRandomNumber(min, max) {
     // console.log(Math.floor(Math.random() * (max - min + 1)) + min)
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
   render() {
-    const lineComponents = this.state.lines.map(line => <Line line={line} handleLineClick={this.handleLineClick}
-    image={line.image}
-    />)
+    const lineComponents = this.state.lines.map((line) => (
+      <Line
+        line={line}
+        handleLineClick={this.handleLineClick}
+        image={line.image}
+        hexagram={this.state.hexagram}
+      />
+    ));
     return (
       <div style={this.styles} className="app">
+        <div className="lines">
         {lineComponents}
-        <IChingText className="divination-text" divinationData={this.state}/>
+        </div>
+        <IChingText className="divination-text" divinationData={this.state} />
       </div>
     );
   }
--- ichingreact/src/iChingData.js
+++ ichingreact/src/iChingData.js
@@ -5,12 +5,12 @@
 
 export const iChingData = {
     lines: [
-        {value: undefined, lineNum: 6, id: 6, image: '../images/nothing.png', name: undefined},
-        {value: undefined, lineNum: 5, id: 5, image: '../images/nothing.png', name: undefined},
-        {value: undefined, lineNum: 4, id: 4, image: '../images/nothing.png', name: undefined},
-        {value: undefined, lineNum: 3, id: 3, image: '../images/nothing.png', name: undefined},
-        {value: undefined, lineNum: 2, id: 2, image: '../images/nothing.png', name: undefined},
-        {value: undefined, lineNum: 1, id: 1, image: '../images/nothing.png', name: undefined},
+        {value: undefined, transformValue: undefined, lineNum: 6, id: 6, image: '../images/nothing.png', transformImage: '../images/nothing.png', name: undefined, transformName: undefined},
+        {value: undefined, transformValue: undefined, lineNum: 5, id: 5, image: '../images/nothing.png', transformImage: '../images/nothing.png', name: undefined, transformName: undefined},
+        {value: undefined, transformValue: undefined, lineNum: 4, id: 4, image: '../images/nothing.png', transformImage: '../images/nothing.png', name: undefined, transformName: undefined},
+        {value: undefined, transformValue: undefined, lineNum: 3, id: 3, image: '../images/nothing.png', transformImage: '../images/nothing.png', name: undefined, transformName: undefined},
+        {value: undefined, transformValue: undefined, lineNum: 2, id: 2, image: '../images/nothing.png', transformImage: '../images/nothing.png', name: undefined, transformName: undefined},
+        {value: undefined, transformValue: undefined, lineNum: 1, id: 1, image: '../images/nothing.png', transformImage: '../images/nothing.png', name: undefined, transformName: undefined},
            ],
     imagePaths: {
         oldYin: '../images/old_yin.png',
@@ -25,7 +25,17 @@ export const iChingData = {
         },
         lower: {
             value: undefined
+        },
+        transformedUpper: {
+            value: undefined
+        },
+        transformedLower: {
+            value: undefined
         }
+    },
+    hexagram: {
+        number: undefined,
+        transformNumber: undefined
     }
 }
 
@@ -48,5 +58,113 @@ export function getTrigram(lowerLineName, middleLineName, upperLineName) {
         return 'water'
     } else if (upperLineName === 'yang' && middleLineName === 'yin' && lowerLineName === 'yin') {
         return 'mountain'
+    }
+}
+
+export function getHexagramNumber(lowerTrigram, upperTrigram) {
+    // console.log('get hex number function running')
+    let hexagramNumbers = {
+        // starting with all hexagrams that have lower trigram of thunder
+        'heaven': {
+            // upper trigrams go here
+            'heaven': 1,
+            'thunder': 34,
+            'water': 5,
+            'mountain': 26,
+            'earth': 11,
+            'wind': 9,
+            'fire': 14,
+            'lake': 43
+        },
+        'thunder': {
+            // upper trigrams go here
+            'heaven': 25,
+            'thunder': 51,
+            'water': 3,
+            'mountain': 27,
+            'earth': 24,
+            'wind': 42,
+            'fire': 21,
+            'lake': 17
+        },
+        'water': {
+            // upper trigrams go here
+            'heaven': 6,
+            'thunder': 40,
+            'water': 29,
+            'mountain': 4,
+            'earth': 7,
+            'wind': 59,
+            'fire': 64,
+            'lake': 47
+        },
+        'mountain': {
+            // upper trigrams go here
+            'heaven': 33,
+            'thunder': 62,
+            'water': 39,
+            'mountain': 52,
+            'earth': 15,
+            'wind': 53,
+            'fire': 56,
+            'lake': 31
+        },
+        'earth': {
+            // upper trigrams go here
+            'heaven': 12,
+            'thunder': 16,
+            'water': 8,
+            'mountain': 23,
+            'earth': 2,
+            'wind': 20,
+            'fire': 35,
+            'lake': 45
+        },
+        'wind': {
+            // upper trigrams go here
+            'heaven': 44,
+            'thunder': 32,
+            'water': 48,
+            'mountain': 18,
+            'earth': 46,
+            'wind': 57,
+            'fire': 50,
+            'lake': 28
+        },
+        'fire': {
+            // upper trigrams go here
+            'heaven': 13,
+            'thunder': 55,
+            'water': 63,
+            'mountain': 22,
+            'earth': 36,
+            'wind': 37,
+            'fire': 30,
+            'lake': 49
+        },
+        'lake': {
+            // upper trigrams go here
+            'heaven': 10,
+            'thunder': 54,
+            'water': 60,
+            'mountain': 41,
+            'earth': 19,
+            'wind': 61,
+            'fire': 38,
+            'lake': 58
+        },
+
+    }
+    // console.log(hexagramNumbers[lowerTrigram][upperTrigram])
+    return hexagramNumbers[lowerTrigram][upperTrigram]
+}
+
+export function getTransformValue(value) {
+    if (value === 6) {
+        return 7
+    } else if (value === 9) {
+        return 8
+    } else {
+        return value
     }
 }
